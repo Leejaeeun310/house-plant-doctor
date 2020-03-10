4ec9fcd1fabc75632e3bb2c93f8b6dc58b2c3295
JaeeunLee <21600539@handong.edu>
2020-03-10 22:10:52 +0900

    first commit

---
+++ pages/StroedPlant.vue
@@ -0,0 +1,84 @@
+<template>
+
+<div class="about">
+      <div><img alt="Vue logo" width="200px" height="150px" src="./logo.png"></div>
+    <!-- <div><img alt="Vue logo" width="200px" height="150px" src="./logo.png"></div> -->
+    <br/> 
+  
+    <router-link to="/StroedPlant" tag="button"><p id="p1"><span style=" font:  bold 1.5em/1em Georgia, serif ;">
+    STORED PLANT</span></p></router-link>
+    <li><router-link to="/About" tag="button"><span style=" font:  bold 1.5em/1em Georgia, serif ;">
+    ABOUT US</span></router-link></li>
+   
+    <br/> 
+
+
+<div><img alt="Vue logo" width="200px" height="150px" src="./normal/1.png"></div>
+
+<div><img alt="Vue logo" width="200px" height="150px" src="./normal/2.png"></div>
+<div><img alt="Vue logo" width="200px" height="150px" src="./normal/3.png"></div>
+<div><img alt="Vue logo" width="200px" height="150px" src="./normal/4.png"></div>
+
+
+<ul v-for='item in items' v-bind:key='item'>
+    <!-- <img v-bind:src="'./normal/' + item + '.png'" v-bind:alt="item">
+    <div><img alt="Vue logo" width="200px" height="150px" src=item></div> -->
+    {{ item.message }}
+   
+
+</ul>
+
+    
+</div>
+
+</template>
+<script>
+
+var i = 0;
+export default {
+    
+
+ data: function() {
+    for(i=0;i<5;i++){
+        i +=i;
+    }
+    
+    return {
+        items: [
+
+      { message: i },
+      { message: 'text' },
+     
+    ]
+  };
+}
+    
+};
+
+</script>
+<style>
+#p1 {background-color:hsl(120,100%,75%);}
+h4 {
+    margin: 25px 50px 75px 10px;
+    }
+
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+h2 {
+ 
+  margin: 0 10px;
+}
+p {
+ 
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}</style>
---
+++ pages/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <section class="container">
+    <div>
+      <div><img alt="Vue logo" width="200px" height="150px" src="./logo.png"></div>
+
+
+      
+      <h1 class="title">
+        Welcome to House Plant Doctor
+      </h1>
+      <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+      </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    
+
+    <router-link to="/StroedPlant" tag="button" type="submit">login</router-link>
+
+    <!-- <button type="submit">login</button> -->
+    <!-- <router-link to="/about" tag="button" type="submit">login</router-link> -->
+  </form>
+     <br/>
+      <li> &copy;Copyright 2020. House Plant Doctor Inc. All Rights Reserved</li>
+    </div>
+  </section>
+</template>
+
+<script>
+//import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from '~/components/AppLogo.vue'
+
+import axios from 'axios';
+
+export default {
+  components: {
+    AppLogo
+  },
+  data: function(){
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function(){
+      //console.log(this.username, this.password);
+      //event.preventDefault();//form의 이동,새로고침을 막는 코드
+      //
+      if(this.username == "LEE" && this.password == "1234"){
+        console.log(this.username, this.password,loading);
+       //methods에서 data로 접근 하기 위해서는 this
+      } 
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password : this.password
+      } 
+      axios.post(url, data)//브라우저에서 서버로 데이터를 주고 받기 위한 라이브러리_post는 데이터를 생성하거나 조작하기 위한 
+       .then(function(response){
+         console.log(response);
+       })
+       .catch(function(error) {
+         console.log(error);
+       });
+       //window.open("https://google.com", "_blank"); 
+    }
+  }
+}
+
+
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+#p1 {background-color:hsl(120,100%,75%);}
+h4 {
+    margin: 25px 50px 75px 10px;
+    }
+
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+h2 {
+ 
+  margin: 0 10px;
+}
+p {
+ 
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+
+</style>
+
